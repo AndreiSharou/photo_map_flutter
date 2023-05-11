2da3dd3f65296f1b9250cec649baada87ecdc594
andrei.sharou <andrei.sharou@itechart-group.com>
2023-05-11 15:46:29 +0200

    App can finally run

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:photo_map_flutter/session/session_cubit.dart';
@@ -5,22 +7,16 @@ import 'package:photo_map_flutter/session/session_cubit.dart';
 import 'app_navigator.dart';
 import 'auth/auth_repository.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // @override
-  // Widget build(BuildContext context) {
-  //   return MaterialApp(
-  //     home: RepositoryProvider(
-  //       create: (context) => AuthRepository(),
-  //       child: LoginView(),
-  //     ),
-  //   );
-  // }
 
   @override
   Widget build(BuildContext context) {
@@ -30,7 +26,7 @@ class MyApp extends StatelessWidget {
         child: BlocProvider(
           create: (context) =>
               SessionCubit(authRepo: context.read<AuthRepository>()),
-          child: AppNavigator(),
+          child: const AppNavigator(),
         ),
       ),
     );
